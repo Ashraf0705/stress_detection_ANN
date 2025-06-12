ee79d4fdc40c8ac793ca0d808d1207f8d97a04a5
Ashraf0705 <smdashraf01@gmail.com>
2025-06-12 14:17:19 +0530

    FIX: Add production URL to ALLOWED_HOSTS and add tensorflow-cpu

--- stress_detection/stress_detection/settings.py
+++ stress_detection/stress_detection/settings.py
@@ -16,7 +16,14 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'localhost',
+    '127.0.0.1',
+]
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
